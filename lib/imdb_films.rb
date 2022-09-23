5a3e6f91fe140d010d910ef8518684e6c31ec3a5
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-23 00:15:24 -0500

    Added req relatives and CLI.new to bin/imdb_films, added call method to cli.rb, added req relative to imdb.films.rb

--- lib/imdb_films.rb
+++ lib/imdb_films.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "imdb_films/version"
+require_relative "imdb_films/cli.rb"
 
 module ImdbFilms
   class Error < StandardError; end
---
+++ lib/imdb_films/cli.rb
@@ -0,0 +1,7 @@
+class ImdbFilms::CLI
+
+def call
+    puts "Welcome to Imdb's top 250 films! Would you like to learn about a movie?"
+end 
+
+end 
